ccf8d6208750db90cac3f2e41d85cf72e29ec9b4
mahbubur1 <mdmahbuborr462@gail.com>
2022-02-16 11:36:25 +0600

    plus minus button funtion add done

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-// case plus button event handler
-document.getElementById('case-plus').addEventListener('click',function(){
+function updateCaseNumber(isIncreasing){
     const caseInput = document.getElementById('case-input');
     const caseInputText = caseInput.value;
-    caseInput.value = parseInt (caseInputText) + 1;
+    if (isIncreasing == true){
+        caseInput.value = parseInt (caseInputText) + 1;
+    }
+    else if(caseInputText > 0){
+        caseInput.value = parseInt (caseInputText) - 1;
+    }
+    
+
+}
+
+
+// case plus button event handler
+document.getElementById('case-plus').addEventListener('click',function(){
+   updateCaseNumber(true);
 })
 
 //case minus button event handler
 document.getElementById('case-minus').addEventListener('click',function(){
-    const caseInput = document.getElementById('case-input');
-    const caseInputText = caseInput.value;
-    caseInput.value = parseInt (caseInputText) - 1;
+   updateCaseNumber(false);
 })
