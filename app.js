897fb2c58a37a9e739df944b1aec7900657c7799
mahbubur1 <mdmahbuborr462@gail.com>
2022-02-16 12:37:56 +0600

    Calculate phone cost using the same shared function done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function updateCaseNumber(isIncreasing){
-    const caseInput = document.getElementById('case-input');
+function updateCaseNumber(product,price, isIncreasing){
+    const caseInput = document.getElementById(product +'-input');
     let caseInputText = caseInput.value;
     if (isIncreasing == true){
         caseInputText= parseInt (caseInputText) + 1;
@@ -8,17 +8,26 @@ function updateCaseNumber(isIncreasing){
         caseInputText = parseInt (caseInputText) - 1;
     }
     caseInput.value = caseInputText;
-    const updateCasePrice = document.getElementById('update-case-price');
-    updateCasePrice.innerText = caseInputText * 59;
+    const updateCasePrice = document.getElementById(product +'-price');
+    updateCasePrice.innerText = caseInputText * price;
 }
+// phone number plus 
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateCaseNumber("phone",1219,true);
+})
+
+// phone number minus
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateCaseNumber("phone",1219, false);
+})
 
 
 // case plus button event handler
 document.getElementById('case-plus').addEventListener('click',function(){
-   updateCaseNumber(true);
+   updateCaseNumber("case",59,true);
 })
 
 //case minus button event handler
 document.getElementById('case-minus').addEventListener('click',function(){
-   updateCaseNumber(false);
+   updateCaseNumber("case",59, false);
 })
